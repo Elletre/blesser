8b759708e5fe0ba47c7a7325f2f9f4d4d76d0299
spacebucks <spaceclive@gmail.com>
2020-04-29 22:18:51 +0400

    dart emozi respond added

--- initializr.py
+++ initializr.py
@@ -1,7 +1,8 @@
 import telebot
+from emoji import emojize
 
 bot = telebot.AsyncTeleBot('')
-
+dartz = emojize(":dart:", use_aliases=True)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -10,10 +11,13 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def greeting_react(message):
-    if message.text == 'Hello':
+    if message.text.lower() == 'hello':
         bot.send_message(message.chat.id, 'Hello to you too')
-    elif message.text == 'Goodbye':
+    elif message.text.lower() == 'goodbye':
         bot.send_message(message.chat.id, 'See ya')
+    elif message.test.lower() == 'luck':
+        bot.send_message(emojize(dartz))
+
 
 bot.polling()
 
